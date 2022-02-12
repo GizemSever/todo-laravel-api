8f3a8fa8271b53af069122e2753e1aceb8a8cbf0
gizemsever <gizemsever68@gmail.com>
2022-02-12 23:54:59 +0300

    Created macro functions for customized response body

--- config/app.php
+++ config/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Providers\ResponseServiceProvider;
 use Illuminate\Support\Facades\Facade;
 
 return [
@@ -177,6 +178,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        ResponseServiceProvider::class
     ],
 
     /*
